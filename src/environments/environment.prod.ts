5449722ff0811ee8c9bf2b552046c98ec3440d76
LU99IS99 <133922082+LU99IS99@users.noreply.github.com>
2025-02-11 12:34:03 -0800

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://farmablebackendtest.xchen83.workers.devv/api'
+    apiUrl: 'https://farmablebackendtest.xchen83.workers.dev/api'
 };
